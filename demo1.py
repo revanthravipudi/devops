0312d1110cbff9ba7ed4b4958a9983164d6f053a
revanth <43996713+revanthravipudi@users.noreply.github.com>
2021-06-08 18:43:00 +0530

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -1,18 +1,1 @@
-k=int(input())
-n=int(input())
-numbers=list(map(int,input().split()))
-b={}
-
-if n%k==0:
-    x=n//k
-    for i in numbers:
-        if i in b:
-            b[i]+=1
-            if b[i]>x:
-                print('NO')
-                break
-        else:
-            b[i]=1
-    print('Yes')
-else:
-    print('NO')
+print("helloworld")
